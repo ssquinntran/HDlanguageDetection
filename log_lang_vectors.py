c6d697c08e2c92fe843cde4e167b2897cab0cd0e
Quinn Tran <ssquinntran@berkeley.edu>
2016-04-21 01:56:59 -0700

    added training set

--- log_lang_vectors.py
+++ log_lang_vectors.py
@@ -10,7 +10,8 @@ import log_random_idx
 #print postprocessed
 
 alphabet = string.lowercase+" "
-filepath = "preprocessed_texts/AliceInWonderland.txt"
+#filepath = "preprocessed_texts/AliceInWonderland.txt"
+
 k = 5000
 N = 30000;
 ordered = 1
@@ -19,7 +20,7 @@ cluster_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # create language vector for Alice in Wonderland made of summed n-gram vectors for each
 # n in cluster_sizes
-def create_log_lang_vec(cluster_sizes, N=N, k=k):
+def create_log_lang_vec(filepath, cluster_sizes, N=N, k=k):
     
     total_lang = np.zeros((1,N))
     # generate english vector
@@ -69,8 +70,7 @@ def recover_frequency(letter_vec, s, array):
 	vec = get_letter_vec(s, letter_vec);
 	return np.dot(vec, array[len(s)-1]);
 
-
-def initialize():
+def initialize(filepath="preprocessed_texts/AliceInWonderland.txt"):
 	n_grams = []
 	lv = random_idx.generate_letter_id_vectors(N,15000);
 	f = open(filepath, "r");
@@ -82,7 +82,7 @@ def initialize():
 	# lang_vectors in sizes 1-8
 	lang_vectors = []
 	for size in cluster_sizes:
-	    lang_vectors.append(create_log_lang_vec([size]))
+	    lang_vectors.append(create_log_lang_vec(filepath,[size]))
 	lang_vectors.insert(0, np.zeros((1,N)))
 
 	# save vectors to file
@@ -95,4 +95,31 @@ def initialize():
 	fwrite.close()
 	fwrite1.close()
 	fwrite2.close()
+
+def train(filepath="preprocessed_texts/alice-only-stream-w-spaces.txt"):
+	n_grams = []
+	lv = random_idx.generate_letter_id_vectors(N,15000);
+	f = open(filepath, "r");
+	text = f.read();
+	text = ''.join([x for x in text if x in alphabet])[0:10000];
+	#for each n, dictionary of key: n gram and value: frequency
+	n_gram_frequencies = [{} for _ in range(len(cluster_sizes) + 1)]
+
+	# lang_vectors in sizes 1-8
+	lang_vectors = []
+	for size in cluster_sizes:
+	    lang_vectors.append(create_log_lang_vec(filepath,[size]))
+	lang_vectors.insert(0, np.zeros((1,N)))
+
+	# save vectors to file
+	fwrite = open("train_lv", "w")
+	fwrite1 = open("train_log_lang_vectors", "w")
+	fwrite2 = open("train_n_gram_frequencies", "w")
+	pickle.dump(lv, fwrite)
+	pickle.dump(lang_vectors, fwrite1)
+	pickle.dump(n_gram_frequencies, fwrite2)
+	fwrite.close()
+	fwrite1.close()
+	fwrite2.close()
+
 
