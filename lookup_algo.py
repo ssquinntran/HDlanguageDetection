8c05d507ae3383e85390a8504a9881825ddb84d2
Quinn Tran <ssquinntran@berkeley.edu>
2016-08-22 16:58:02 -0700

    added em. need threshold

--- lookup_algo.py
+++ lookup_algo.py
@@ -69,48 +69,83 @@ def dict_explain_away(vocab,max_length,text):
 
 # create a threshold > 0 so that if a word really is new 
 # (not connected to any known roots), then add to vocab
-def hard_e_step(word,vocab):
+def hard_e_step(word,vocab,lv,threshold=0):
     cluster_size = len(vocab)
     # if word is > max vocab
     if len(word) < cluster_size:
         cluster_size = len(word)
     potential_words = []
+    word_vector = random_idx.id_vector(N, word, alphabet, lv, ordered)
+
     for i in range(cluster_size-1, -1,-1):
         for key in vocab[i].keys():
-            dot_product = key.vector * word.vector
-            if dot_product > 0:
-                potential_words.append([dot_product, key])
+            key_vector = random_idx.id_vector(N, key, alphabet, lv, ordered)
+            similarity = np.dot(np.transpose(word_vector[0]), key_vector[0])
+            if similarity > threshold:
+                potential_words.append([similarity, key])
     return potential_words
-# ARGH FIX PSEUDOCODE
+
 
 # records frequencies and word(s) in vocab
 def hard_m_step(word,potential_words,vocab):
     sorted(potential_words)
     if not potential_words:
-        vocab[len(word)][word] = 1 # or is it vocab[len(word)-1]
-        return None
+        vocab[len(word)-1][word] = 1
+        return word
     potential_word = potential_words[len(potential_words)-1]
-    vocab[len(word)][potential_word] += 1 # or is it vocab[len(word)-1]
+    #print potential_word
+    # need to add vocab since pulling words out can slice words in a way
+    # that introduces new keys (beginnings/endings)
+    if potential_word[1] not in vocab[len(potential_word)-1]:
+        vocab[len(potential_word)-1][potential_word[1]] = 1
+    else:
+        vocab[len(potential_word)-1][potential_word[1]] += 1
     return potential_word
 
-def hard_em_discover_words(processed_indices,vocab,text):
+
+def hard_em_discover_words(processed_indices,text,vocab,lv):
     # find the left over intervals
     undiscovered = []
+    # undiscovered at beginning of text
+    if processed_indices[0][0] > 0:
+        undiscovered.append([0,processed_indices[0][0]])
     for i in range(0,len(processed_indices)-1):
         start = processed_indices[i][1]
         end = processed_indices[i+1][0]-1
         if start < end:
             undiscovered.append([start, end])
             #print text[start:end]
-    print undiscovered
+    # undiscovered at end of text
+    if processed_indices[len(processed_indices)-1][1] < len(text)-1:
+        undiscovered.append([processed_indices[len(processed_indices)-1][1],len(text)-1])
 
     processed = []
     for pair in undiscovered:
         word = text[pair[0]:pair[1]]
-        potential_words = hard_e_step(word,vocab)
+        potential_words = hard_e_step(word,vocab,lv)
         potential_word = hard_m_step(word,potential_words,vocab)
-        
-    return undiscovered
+        processed.append([pair,potential_word])
+    return processed
+
+
+def record_results(text, processed_indices,discovered_words,output_url="output/explain_away_results.txt"):
+    fwrite = open(output_url,"w")
+    discovered_index = 0
+    # discovered at beginning of text
+    if processed_indices[0][0] > 0:
+        fwrite.write(discovered_words[0][1][1] + " ")
+        discovered_index += 1
+    for i in range(0,len(processed_indices)-1):
+        fwrite.write(text[processed_indices[i][0]:processed_indices[i][1]] + " ")
+        start = processed_indices[i][1]
+        end = processed_indices[i+1][0]-1
+        if start < end:
+            fwrite.write(discovered_words[discovered_index][1][1] + " ")
+            discovered_index += 1
+    # discovered at end of text
+    if processed_indices[len(processed_indices)-1][1] < len(text)-1:
+        fwrite.write(discovered_words[len(discovered_words)-1][1][1])
+    fwrite.close()
 
 def seed():
     filepath = "preprocessed_texts/english/alice-only-spaced.txt"
@@ -125,16 +160,14 @@ def seed():
 
     processed = tuples_to_text(processed_indices, text)
 
-    fwrite = open("intermediate/processed_dict_explain_away_results","w")
+    fwrite = open("intermediate/processed_dict_explain_away_results.txt","w")
     fwrite.write(processed)
     fwrite.close()
 
     # now for the em
     # not necessary in seeding phase. 
-    hed = hard_em_discover_words(processed_indices, vocab, text)
-    #file = open("../output/processed_array_explain_away_results","w")
-    #file.write(hed)
-    #file.close()
+    discovered_words = hard_em_discover_words(processed_indices, text, vocab, lv)
+    record_results(text, processed_indices,discovered_words,"output/explain_away_results.txt")
 
     # save data to file
     lvu.write_data_structures([lv, lang_vectors, n_gram_frequencies, vocab_vec, vocab], \
